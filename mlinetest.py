be5de1dc201248018225d5054dcc7479fb44d612
Zwelckovich <54358761+Zwelckovich@users.noreply.github.com>
2025-10-20 07:45:32 +0200

    Add files via upload

--- mlinetest.py
+++ mlinetest.py
@@ -14,25 +14,20 @@ from emerge.plot import plot_sp
 
 SIMULATION_PARAMS = {
     # Geometry parameters
-    "W": 90,  # Width in um
-    "H": 101.6,  # Height in um
-    "T": 5,  # Thickness in um
-    "length": 560,  # Length of the MLine in um (560 μm)
-    "wgnd_multiplier": 20,  # Ground plane width = W * wgnd_multiplier
+    "TRACK_W": 3,  # Width
+    "TRACK_T": 0.05,  # Thickness
+    "SUB_T": 1.55,  # Height
+    "SUB_L": 100,  # Length Substrate
     # Frequency parameters
-    "f_start": 1e9,  # Start frequency in Hz
-    "f_stop": 110e9,  # Stop frequency in Hz
-    "f_step": 1e9,  # Frequency step size in Hz (1 GHz steps)
+    "F_START": 100e6,  # Start frequency in Hz
+    "F_STOP": 5e9,  # Stop frequency in Hz
+    "F_STEP": 100e6,  # Frequency step size in Hz (1 GHz steps)
     # Material parameters
-    "ep_r": 9.6,  # Relative permittivity
-    "cond": 4.561e7,  # Conductivity in S/m
-    "tanD": 0.0003,  # Loss tangent
+    "ep_r": 4.5,  # Relative permittivity
+    "cond": 5.800e7,  # Conductivity in S/m
+    "tanD": 0.02,  # Loss tangent
     "rough": 0.2e-6,  # Surface roughness in meters
     "f_epr_tand": 10e9,  # Frequency for Epsilon_r and tanD
-    # Derived parameters
-    "rho": lambda: 1 / SIMULATION_PARAMS["cond"],  # Resistivity in Ohm*m
-    "wgnd": lambda: SIMULATION_PARAMS["W"]
-    * SIMULATION_PARAMS["wgnd_multiplier"],  # Ground plane width
     # Units
     "m": 1,  # m, Definition
     "mm": 1e-3,  # mm, Definition
@@ -66,20 +61,20 @@ tracemat = em.Material(
 
 # Next we create the PCB designer class instance.
 pcb = em.geo.PCB(
-    thickness=SIMULATION_PARAMS["H"],
-    unit=SIMULATION_PARAMS["um"],
+    thickness=SIMULATION_PARAMS["SUB_T"],
+    unit=SIMULATION_PARAMS["mm"],
     material=pcbmat,
     trace_material=tracemat,
-    trace_thickness=SIMULATION_PARAMS["T"],
+    trace_thickness=SIMULATION_PARAMS["TRACK_T"],
 )
 
 # We can use this function to compute the microstripline impedance. Right now (version 1.1) it just assumes top vs bottom layer.
 # w0 = pcb.calc.z0(50)
 
 # We start a new simple microstripline at xy=(0,0) and go Lfeed forward.
-pcb.new(x=0, y=0, width=SIMULATION_PARAMS["W"], direction=(0, 1)).store("p1").straight(
-    SIMULATION_PARAMS["length"]
-).store("p2")
+pcb.new(x=0, y=0, width=SIMULATION_PARAMS["TRACK_W"], direction=(0, 1)).store(
+    "p1"
+).straight(SIMULATION_PARAMS["SUB_L"]).store("p2")
 
 # Then finally we generate all our paths.
 # They will be returned in the following order
@@ -90,22 +85,31 @@ trace = pcb.compile_paths()
 
 # After generation of all geometries we can determine the bounds. We will add 20mm of margin to the left and back of
 # our PCB domain. We start at the front and end at the right.
-pcb.determine_bounds(leftmargin=900, topmargin=0, rightmargin=900, bottommargin=0)
+pcb.determine_bounds(
+    leftmargin=5 * 10 * SIMULATION_PARAMS["TRACK_W"] / 2,
+    topmargin=0,
+    rightmargin=5 * 10 * SIMULATION_PARAMS["TRACK_W"] / 2,
+    bottommargin=0,
+)
 
 # We add a ground plane on the bottom.
 ground = pcb.plane(pcb.z(0))
 
 # We add two modal port surfaces at the nodes p1 and p2
 mp1 = pcb.modal_port(
-    pcb.load("p1"), height=2 * SIMULATION_PARAMS["W"], width_multiplier=6
+    pcb.load("p1"),
+    height=6 * SIMULATION_PARAMS["SUB_T"],
+    width=10 * SIMULATION_PARAMS["TRACK_W"],
 )
 mp2 = pcb.modal_port(
-    pcb.load("p2"), height=2 * SIMULATION_PARAMS["W"], width_multiplier=6
+    pcb.load("p2"),
+    height=6 * SIMULATION_PARAMS["SUB_T"],
+    width=10 * SIMULATION_PARAMS["TRACK_W"],
 )
 
 # Finally we generate the PCB, top air and bottom air.
 diel = pcb.generate_pcb()
-air_top = pcb.generate_air(height=2 * SIMULATION_PARAMS["W"])
+air_top = pcb.generate_air(height=3 * 6 * SIMULATION_PARAMS["SUB_T"])
 
 # With Commit geometry we say: here we are done generating our model.
 m.commit_geometry()
@@ -114,7 +118,13 @@ m.commit_geometry()
 m.view()
 
 # We set the frequency range from 1GHz to 110GHz in 110 steps.
-m.mw.set_frequency_range(1e9, 110e9, 110)
+npoints = (
+    SIMULATION_PARAMS["F_STOP"] - SIMULATION_PARAMS["F_START"]
+) / SIMULATION_PARAMS["F_STEP"] + 1
+m.mw.set_frequency_range(
+    fmin=SIMULATION_PARAMS["F_START"], fmax=SIMULATION_PARAMS["F_STOP"], Npoints=int(npoints)
+)
+m.mw.set_resolution(0.25)
 
 # We don't use any manual refinement steps to illustrate the power of
 # Adaptive Mesh refinement.
@@ -142,7 +152,7 @@ p2 = m.mw.bc.ModalPort(mp2, 2, modetype="TEM")
 # We set show_mesh to True so we can see the progress of refinement for the purspose of this example.
 # This halts the simulation so we have to click away the window to proceed.
 # You can see that more nodes are added around the signal traces because the E-field error is highest
-m.adaptive_mesh_refinement(frequency=20e9, show_mesh=False, growth_rate=5)
+# m.adaptive_mesh_refinement(frequency=2.5e9, show_mesh=False, growth_rate=5)
 
 # We can view the improvement in the refined mesh.
 m.view(plot_mesh=True)
